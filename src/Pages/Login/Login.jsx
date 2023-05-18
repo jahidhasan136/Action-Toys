7bda5d262e774e7bbce2ae3d6459749932400a9a
Jahid Hasan <jh3495689@gmail.com>
2023-05-19 02:26:09 +0600

    added a register page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import { FaGoogle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content">
-                <div className="card flex-shrink-0  w-[900px] max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                <div className="card flex-shrink-0  w-[450px] shadow-2xl bg-base-100">
+                    <h1 className="text-4xl text-center pt-3 font-bold">Please Login !!</h1>
+                    <form className="card-body pb-2">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="email" placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="password" placeholder="password" className="input input-bordered" />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
@@ -24,6 +27,11 @@ const Login = () => {
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Login</button>
                         </div>
+                        <small>Dont Have an account? <Link to="/register" className="font-bold btn-link">Register</Link></small>
+                    </form>
+                    <p className="text-center text-xl font-bold">or</p>
+                    <div className="p-5 text-center">
+                        <button className="btn bg-blue-600 flex gap-3 mx-auto"><FaGoogle className="text-lg"></FaGoogle> Sign in with Google</button>
                     </div>
                 </div>
             </div>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content">
+                <div className="card flex-shrink-0  w-[450px] shadow-2xl bg-base-100">
+                    <h1 className="text-4xl text-center pt-3 font-bold">Please Register !!</h1>
+                    <form className="card-body pb-2">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="name" placeholder="name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" placeholder="photo url" className="input input-bordered" />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                        <small className="p-4">Already Have an account? <Link to="/login" className="font-bold btn-link">Login</Link></small>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
